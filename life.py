21f06e5b5acf2d0845c7d76aaab9068ae2178a7c
Eric Dennison <ericd@netdenizen.com>
2018-09-17 13:58:07 -0400

    Updated from Brython Server: 9/17/2018 1:58:07 PM

--- life.py
+++ life.py
@@ -1,3 +1,9 @@
 from ggame import CircleAsset, Color, LineStyle, Sprite
+from ggame import App
 
 redcircle = CircleAsset(5, LineStyle(0,0), Color(0xff0000))
+
+Sprite(redcircle, (100,100))
+
+myapp = App()
+myapp.run()
