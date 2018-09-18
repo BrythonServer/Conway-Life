7698180b30e1df3adde80c6abad28ddf0976af14
Eric Dennison <ericd@netdenizen.com>
2018-09-18 12:41:22 -0400

    Updated from Brython Server: 9/18/2018 12:41:22 PM

--- life.py
+++ life.py
@@ -8,7 +8,7 @@ black = Color(0,1.0)
 CELLDIAMETER = 10
 
 
-class Cell(Sprite):
+class Cell(object):
     freereds = []
     freeblues = []
     reds = []
@@ -17,11 +17,48 @@ class Cell(Sprite):
     bluecircle = CircleAsset(CELLDIAMETER/2, LineStule(0, black), blue)
     
     def __init__(self, logicalpos):
-        super().__init__(self.getred(), 
-            (logicalpos[0]*CELLDIAMETER, logicalpos[1]*CELLDIAMETER))
+        if self.reds:
+            self.cell = self.freereds.pop()
+            self.cell.visible = True
+        else:
+            self.cell = Sprite(self.redcircle, (0,0))
+        self.cell.position = (logicalpos[0]*CELLDIAMETER, logicalpos[1]*CELLDIAMETER)
+        self.reds.append(self.cell)
+        self.age = 0
+
+    def ageoneday(self):
+        # convert from red to blue
+        if self.age == 0:
+            newcell = None
+            if self.freeblues:
+                newcell = self.freeblues.pop()
+                newcell.visible = True
+            else:
+                newcell = Sprite(self.bluecircle, (0,0))
+            # move a blue cell to the actie list
+            self.blues.append(newcell)
+            self.cell.visible = False  
+            # move the red cell to free list
+            self.freereds.append(self.cell)
+            self.reds.remove(self.cell)
+            newcell.position = self.cell.position
+            self.cell = newcell
+            self.age = 1
             
-    def getred(self):
-        if freereds
+    def die(self):
+        # make invisible and move sprite to free list
+        if self.age == 1
+            freelist = self.freeblues
+            activelist = self.blues
+        else:
+            freelist = self.freereds
+            activelist = self.reds
+        self.cell.visible = False
+        activelist.remove(self.cell)
+        freelist.append(self.cell)
+            
+
+
 
 Sprite(redcircle, (100,100))
 
