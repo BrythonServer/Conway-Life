a534e45d91da915ad20bb84a80433d200e7369b9
Eric Dennison <ericd@netdenizen.com>
2018-09-18 12:06:14 -0400

    Updated from Brython Server: 9/18/2018 12:06:14 PM

--- life.py
+++ life.py
@@ -5,7 +5,23 @@ red = Color(0xff0000, 1.0)
 blue = Color(0xff0000, 1.0)
 black = Color(0,1.0)
 
-redcircle = CircleAsset(5, LineStyle(0, black), red)
+CELLDIAMETER = 10
+
+
+class Cell(Sprite):
+    freereds = []
+    freeblues = []
+    reds = []
+    blues = []
+    redcircle = CircleAsset(CELLDIAMETER/2, LineStyle(0, black), red)
+    bluecircle = CircleAsset(CELLDIAMETER/2, LineStule(0, black), blue)
+    
+    def __init__(self, logicalpos):
+        super().__init__(self.getred(), 
+            (logicalpos[0]*CELLDIAMETER, logicalpos[1]*CELLDIAMETER))
+            
+    def getred(self):
+        if freereds
 
 Sprite(redcircle, (100,100))
 
