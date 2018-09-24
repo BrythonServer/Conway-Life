cb93d2c5c4f0bc4cf7503338333a697cb5e6091b
Eric Dennison <ericd@netdenizen.com>
2018-09-24 13:41:10 -0400

    Updated from Brython Server: 9/24/2018 1:41:10 PM

--- life.py
+++ life.py
@@ -37,6 +37,7 @@ def NewCellAt(coords):
     except:
         newcell = (Sprite(redcircle,(0,0)), Sprite(bluecircle,(0,0)))
     livecells[coords] = newcell
+    print(len(livecells), livecells)
     newcell[0].visible = True
     newcell[1].visible = False
     newcell[0].position = newcell[1].position = pfroml(coords)
@@ -61,6 +62,7 @@ def MakeAGlider(pos):
 
 for i in range(5):
     MakeAGlider((i*6, i*2))
+print(len(neighborsof), len(livecells), len(deadcells))
 
 
 def step():
