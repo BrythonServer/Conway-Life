ddf7341cdd80d8f8cf4c2a57b79311fbc2aced70
Eric Dennison <ericd@netdenizen.com>
2018-09-26 12:26:00 -0400

    Updated from Brython Server: 9/26/2018 12:26:00 PM

--- life.py
+++ life.py
@@ -41,9 +41,9 @@ def NewCellAt(coords):
         except:
             newcell = (Sprite(redcircle,(0,0)), Sprite(bluecircle,(0,0)))
         livecells[coords] = newcell
+        newcell[0].position = newcell[1].position = pfroml((coords[0]+screenoffset[0], coords[1]+screenoffset[1]))
         newcell[0].visible = True
         newcell[1].visible = False
-        newcell[0].position = newcell[1].position = pfroml((coords[0]+screenoffset[0], coords[1]+screenoffset[1]))
 
 # return number of live neighbors and list of empty neighbors
 def ScanCell(coords):
