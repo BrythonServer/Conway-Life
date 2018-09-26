2231bc19cee5e2734fb99224c5d17a069ca4435c
Eric Dennison <ericd@netdenizen.com>
2018-09-26 12:46:04 -0400

    Updated from Brython Server: 9/26/2018 12:46:04 PM

--- life.py
+++ life.py
@@ -112,7 +112,7 @@ def MouseDown(event):
     global screenoffset
     mousedown = True
     pos = lfromp((event.x, event.y))
-    NewCellAt((pos[0]-screenoffset[0], pos[1]-screenoffset[1]))
+    NewCellAt(pos)
 
 def MouseUp(event):
     global mousedown
@@ -122,7 +122,7 @@ def MouseMove(event):
     global screenoffset
     if mousedown:
         pos = lfromp((event.x, event.y))
-        NewCellAt((pos[0]-screenoffset[0], pos[1]-screenoffset[1]))
+        NewCellAt(pos)
         
 def Spacebar(event):
     global running
